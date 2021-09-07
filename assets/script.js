dae13ee0c971b97e236b1341cd5792eab61f08b0
TKShadowBlade <katanablack85@gmail.com>
2021-09-06 23:32:19 -0400

    JS test coding for background images

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+document.body.style.backgroundImage = "url('https://afar-production.imgix.net/uploads/images/afar_post_headers/images/iDlkx4v5ic/original_Rocky_20Mountain_20National_20Park.jpg?auto=compress,format&fit=crop&crop=top&lossless=true&w=1600&h=700')";
+
+//(function() {
+    //var rotator = document.body.style.backgroundImage;  
+    //var imageDir = "C:/Users/Anthony Korneagay Jr/bootcamp/Project-One/assets/Pictures";                         
+   // var delayInSeconds = 5;                            
+
+   // var images = ['badlands-national-park.jpg', 'denali-national-park.jpg', 'gibbon-falls.jpg', 'rocky-mountain-two-moose.jpg', 'rocky-mountain-wildflowers.jpg'];
+
+   // var num = 0;
+  //  var changeImage = function() {
+       // var len = images.length;
+       // rotator.src = imageDir + images[num++];
+       // if (num == len) {
+       //     num = 0;
+     //   }
+  //  };
+  //  setInterval(changeImage, delayInSeconds * 100);
+//})();
